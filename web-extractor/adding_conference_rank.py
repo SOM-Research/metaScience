3785b1dccc2e14a722a2e25dc8622b1c176f55ee
valeriocos <valerio.cosentino@gmail.com>
2014-09-01 13:29:31 +0200

    bug fixing in conference rank

--- web-extractor/adding_conference_rank.py
+++ web-extractor/adding_conference_rank.py
@@ -32,24 +32,24 @@ COMPUTER_SCIENCE_CONFERENCE_RANK = 'http://lipn.univ-paris13.fr/~bennani/CSRank.
 driver = webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\chromedriver.exe')
 
 
-def get_ranked_conferences_core():
-    driver.get(COMPUTER_SCIENCE_CONFERENCE_RANK)
-    time.sleep(5)
-    ranks = driver.find_elements_by_tag_name("table")
-    return ranks
-
-
 def add_proceedings_rank(cnx):
     cursor = cnx.cursor()
-    ranks = get_ranked_conferences_core()
-    for rank in ranks:
+    driver.get(COMPUTER_SCIENCE_CONFERENCE_RANK)
+    #iterate over the conferences of rank A* (0), A (1), B (2), C (3)
+    for i in [0, 1, 2, 3]:
+        driver.find_elements_by_tag_name("li")[i].find_element_by_partial_link_text("Rank").click()
+        rank = driver.find_elements_by_tag_name("table")[i]
         conferences = rank.find_elements_by_tag_name("tr")
         for conf in conferences:
             try:
                 core_acronym = conf.find_element_by_xpath("./td[1]").text
                 core_rank = conf.find_element_by_xpath("./td[3]").text
-                query = "UPDATE aux_dblp_proceedings SET rank = %s WHERE source = %s " \
-                        "AND type IN ('conference', 'symposium')"
+
+                #exceptions
+                if core_acronym == "FSE":
+                    core_acronym = "'SIGSOFT FSE', 'ESEC/SIGSOFT FSE'"
+
+                query = "UPDATE aux_dblp_proceedings SET rank = %s WHERE source IN (%s) "
                 arguments = [core_rank, core_acronym]
                 cursor.execute(query, arguments)
                 cnx.commit()
