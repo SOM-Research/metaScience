f4bf2c153844a6f246923f97ded947d66f51e51d
valeriocos <valerio.cosentino@gmail.com>
2015-06-22 13:58:12 +0200

    adding metrics for authors (distinct number of co-authors, total number of conferences and average number of conference per year for a given author)

--- queries/author_metrics.sql
+++ queries/author_metrics.sql
@@ -74,11 +74,35 @@ group by pub.id) as pub_info
 group by pub_info.year;
 
 /* number of co-authors per year */
-select author_id, author, sum(coauthors), year
+select author_id, author, sum(coauthors)
 from (
 select pub.id, pub.year, title, airn.author_id, airn.author, if(max(author_num) + 1 = 1, 0, max(author_num)) as coauthors
 from dblp_pub_new pub join dblp_authorid_ref_new airn on pub.id = airn.id
 join dblp_author_ref_new arn on pub.id = arn.id
-where airn.author_id = 636270 and pages is not null
+where airn.author_id = 636270
 group by pub.id) as pub_info
 group by pub_info.year;
+
+/* distinct number of co-authors for a given author */
+select count(*) as total
+from (
+select author_id
+from
+(select pub.id, airn.author_id, airn.author
+from dblp_pub_new pub join dblp_authorid_ref_new airn on pub.id = airn.id
+where airn.author_id <> 636270) as co_authors
+join
+(select pub.id
+from dblp_pub_new pub join dblp_authorid_ref_new airn on pub.id = airn.id
+where airn.author_id = 636270) as author
+on co_authors.id = author.id
+group by author_id) as total_coauthors;
+
+/* total number of conferences and average number of conference per year for a given author */
+select sum(number_of_conferences) as total_conferences, round(avg(number_of_conferences),2) as average_conferences
+from (
+select airn.author_id, airn.author, year, count(year) as number_of_conferences
+from dblp_pub_new pub join dblp_authorid_ref_new airn 
+on pub.id = airn.id
+where source IS NOT NULL and airn.author_id = 636270 and type = 'inproceedings' and title NOT LIKE '%workshop%'
+group by year) as conferences;
